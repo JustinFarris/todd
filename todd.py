e6126b832cdb998910288d3ca6667f7109509f19
todd@bang.bang <todd@bang.bang>
2018-07-22 18:43:55 +0000

    MORE SONGS!!!

--- todd.py
+++ todd.py
@@ -38,7 +38,10 @@ while True:
         if playing == False:
             print('White/No pressed - White/No button disabled until song completes')
             playing = True
-            nopid = subprocess.Popen(["touch /tmp/playing && play -q Songs/BangOriginal.mp3 && rm /tmp/playing"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+            randomBang = random.choice(os.listdir("/home/pi/todd/Songs/"))
+#            print(randomBang)
+#nopid = subprocess.Popen(["touch /tmp/playing && play -q Songs/BangOriginal.mp3 && rm /tmp/playing"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+            nopid = subprocess.Popen(['touch /tmp/playing && play -q "/home/pi/todd/Songs/' + randomBang + '" && rm /tmp/playing'], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
             time.sleep(0.2)
     if yes_state == False and no_state == True:
         print('Yellow/Yes pressed - button still enabled')
